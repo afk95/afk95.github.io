db7a2b664f2dbe28d1795a72f74a41197f574b7c
vipcw95 <vipcw95@gmail.com>
2024-10-10 10:30:22 +0800

    init

---
+++ static/js/chunk-2d2091f0.1ecac4d5.js
@@ -0,0 +1,7 @@
+/*!
+ *  build: vue-admin-better 
+ *  vue-admin-better.com 
+ *  https://gitee.com/chu1204505056/vue-admin-better 
+ *  time: 2024-10-10 10:02:33
+ */
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2091f0"],{a869:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t._self._c;return e("div",{staticClass:"menu1-container"},[e("el-alert",{attrs:{closable:!1,title:"嵌套路由 1",type:"success"}},[e("router-view")],1)],1)},l=[],a=n("2877"),c={},u=Object(a["a"])(c,s,l,!1,null,null,null);e["default"]=u.exports}}]);
---
+++ static/js/chunk-8b2d4c42.108fd4a2.js
@@ -0,0 +1,7 @@
+/*!
+ *  build: vue-admin-better 
+ *  vue-admin-better.com 
+ *  https://gitee.com/chu1204505056/vue-admin-better 
+ *  time: 2024-10-10 10:02:33
+ */
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-8b2d4c42"],{"789c":function(t,c,n){},8250:function(t,c,n){"use strict";n.r(c);var s=function(){var t=this,c=t._self._c;return c("div",{staticClass:"menu1-1-1-container"},[c("el-alert",{attrs:{closable:!1,title:"嵌套路由 1-1-1",type:"success"}})],1)},e=[],a=(n("adda"),n("2877")),u={},i=Object(a["a"])(u,s,e,!1,null,"566bf294",null);c["default"]=i.exports},adda:function(t,c,n){"use strict";n("789c")}}]);
