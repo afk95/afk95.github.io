8f371157e16864a17bc108de121a59c4d089e743
vipcw95 <vipcw95@gmail.com>
2024-11-01 11:20:07 +0800

    init

--- static/js/chunk-2d238672.6d67cadf.js
+++
@@ -1,7 +0,0 @@
-/*!
- *  build: vue-admin-better 
- *  vue-admin-better.com 
- *  https://gitee.com/chu1204505056/vue-admin-better 
- *  time: 2024-10-10 11:19:27
- */
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d238672"],{feda:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t._self._c;return e("div",{staticClass:"test-container"},[e("el-divider",{attrs:{"content-position":"left"}},[t._v("你可以在这里写demo")])],1)},a=[],o={name:"Test",data(){return{show:!0}},created(){},mounted(){},methods:{}},d=o,i=n("2877"),r=Object(i["a"])(d,s,a,!1,null,null,null);e["default"]=r.exports}}]);
