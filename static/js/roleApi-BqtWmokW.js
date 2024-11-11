6a06f5b7cb5f722f825368bbccc89ab4ce645f1c
--global <vipcw95@gmail.com>
2024-11-11 13:59:29 +0800

    init
    init

--- static/js/roleApi-BqtWmokW.js
+++
@@ -1,2 +0,0 @@
-import{a as s}from"./axios-DWa7EUUt.js";const e={getRoleList:o=>s("postjson","/getRoleList",o),getInfo:o=>s("postjson","/getRoleInfo",o),saveInfo:o=>s("postjson","/postRoleInfo",o),saveXiulianStatus:o=>s("postjson","/postXiulianStatus",o),postModifyRole:o=>s("postjson","/postModifyRole",o)};export{e as a};
-//# sourceMappingURL=roleApi-BqtWmokW.js.map
