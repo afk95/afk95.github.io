6e03dddab29c64602564a7e3c4d605d5de4c10fe
--global <vipcw95@gmail.com>
2024-11-12 08:58:56 +0800

    update rank
    update rank

--- static/js/Sect-BXt5-n8g.js
+++
@@ -1,2 +0,0 @@
-import{a as s}from"./roleApi-CYquQcIH.js";import{_ as a,r as o,o as c,t as n,c as r,g as i}from"./index-CCGbl0Gs.js";import"./axios-Dtn0P87O.js";const p={name:"Sect",setup(){const t=o({});c(async()=>{await e()});const e=async()=>{await s.getInfo({uid:"1"})};return{...n(t)}}},_={class:"sect"};function d(t,e,f,m,u,l){return i(),r("div",_,"宗门")}const x=a(p,[["render",d],["__scopeId","data-v-d3e54d6c"]]);export{x as default};
-//# sourceMappingURL=Sect-BXt5-n8g.js.map
--- static/js/transItemText-6-xW7tNd.js
+++
@@ -1,2 +0,0 @@
-const s=e=>{let n=[];for(let t=0;t<e.length;++t){let a=e[t];switch(e[t].name){case"waigong":a.nameText="外攻";break;case"neigong":a.nameText="内攻";break}n.push(a)}return n},r=e=>({0:"道具",1:"任务",2:"武器",3:"防具",4:"饰品"})[e];export{r as a,s as t};
-//# sourceMappingURL=transItemText-6-xW7tNd.js.map
